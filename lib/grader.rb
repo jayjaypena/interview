7934eb5bd2e124b061302213e9cdf0c9d6ab7007
Jayjay <jj.pena@medcheck.com.ph>
2025-01-24 12:38:25 +0800

    feat: implement Grader and Student class

--- lib/grader.rb
+++ lib/grader.rb
@@ -1,7 +1,29 @@
-require "csv"
-require "student"
+# lib/grader.rb
+require 'csv'
+require 'student'
 
 class Grader
-  def initialize(file)
+  attr_reader :students
+
+  def initialize(file_path)
+    @students = []
+    read_csv(file_path)
+  end
+
+  private
+  def read_csv(file_path)
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+      grades = {
+        english: row[:english],
+        math: row[:math],
+        physics: row[:physics]
+      }
+      student = Student.new(
+        first_name: row[:first_name],
+        last_name: row[:last_name],
+        grades: grades
+      )
+      @students << student
+    end
   end
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,2 +1,17 @@
+# lib/student.rb
 class Student
+  attr_reader :first_name, :last_name, :grades, :average_grade
+
+  def initialize(first_name:, last_name:, grades:)
+    @first_name = first_name
+    @last_name = last_name
+    @grades = grades
+    calculate_average
+  end
+
+  private
+  def calculate_average
+    total_grades = grades.values.map(&:to_i)
+    @average_grade = (total_grades.sum.to_f / total_grades.size).round
+  end
 end
